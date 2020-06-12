919fc5ff0203ab55084a7d6aaf7ab6f3f0359e46
Squareben1 <bengittins@live.com>
2020-06-12 16:14:31 +0100

    projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,6 +14,55 @@ class Projects extends React.Component {
               <div class="text">Dino Run</div>
             </div>
           </div>
+          <div className="projectContainer">
+            <h1>DINO RUN</h1>
+            <img src={DinoRun} className="projectImage" alt="Dino Run" />
+            <div class="middle">
+              <div class="text">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Possimus voluptate obcaecati nesciunt porro laudantium, autem
+                  expedita cum fugit cupiditate itaque debitis sint, tenetur
+                  accusamus, omnis!
+                </p>
+
+                <p>
+                  Est, dolorum, inventore? Soluta illo necessitatibus facilis
+                  omnis nam ipsam, laudantium voluptas veniam ipsa. Ea debitis,
+                  explicabo! Fugit repudiandae ex unde assumenda, numquam
+                  aliquam architecto?
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="projectContainer">
+            <h1>DINO RUN</h1>
+            <img src={DinoRun} className="projectImage" alt="Dino Run" />
+            <div class="middle">
+              <div class="text">Dino Run</div>
+            </div>
+          </div>
+          <div className="projectContainer">
+            <h1>DINO RUN</h1>
+            <img src={DinoRun} className="projectImage" alt="Dino Run" />
+            <div class="middle">
+              <div class="text">Dino Run</div>
+            </div>
+          </div>
+          <div className="projectContainer">
+            <h1>DINO RUN</h1>
+            <img src={DinoRun} className="projectImage" alt="Dino Run" />
+            <div class="middle">
+              <div class="text">Dino Run</div>
+            </div>
+          </div>
+          <div className="projectContainer">
+            <h1>DINO RUN</h1>
+            <img src={DinoRun} className="projectImage" alt="Dino Run" />
+            <div class="middle">
+              <div class="text">Dino Run</div>
+            </div>
+          </div>
         </div>
       </>
     );
