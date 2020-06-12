9b2588c9dbd3eebb985974a8c6205711d016306f
Squareben1 <bengittins@live.com>
2020-06-12 12:56:41 +0100

    updated skills section

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
-import Skills from './Skills'
+import Skills from "./Skills";
 
 class About extends React.Component {
   handleLinkClick = (url) => {
@@ -11,45 +11,48 @@ class About extends React.Component {
     return (
       <>
         <section className="aboutSection">
-          <Skills />
-          <div className="aboutTextContainer">
-          <h2>Full Stack Developer</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus
-              voluptate obcaecati nesciunt porro laudantium, autem expedita cum
-              fugit cupiditate itaque debitis sint, tenetur accusamus, omnis!
-            </p>
+          <div className="aboutContainer">
+            <Skills />
+            <div className="aboutTextContainer">
+              <h2>Full Stack Developer</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Possimus voluptate obcaecati nesciunt porro laudantium, autem
+                expedita cum fugit cupiditate itaque debitis sint, tenetur
+                accusamus, omnis!
+              </p>
 
-            <p>
-              Est, dolorum, inventore? Soluta illo necessitatibus facilis omnis
-              nam ipsam, laudantium voluptas veniam ipsa. Ea debitis, explicabo!
-              Fugit repudiandae ex unde assumenda, numquam aliquam architecto?
-            </p>
+              <p>
+                Est, dolorum, inventore? Soluta illo necessitatibus facilis
+                omnis nam ipsam, laudantium voluptas veniam ipsa. Ea debitis,
+                explicabo! Fugit repudiandae ex unde assumenda, numquam aliquam
+                architecto?
+              </p>
 
-            <p>
-              Beatae minima, ipsam nisi rerum commodi. Culpa quod quibusdam,
-              odit ut! Reprehenderit officiis sint suscipit, neque, mollitia
-              minus? Ab earum cum nam at, quos id!
-            </p>
-            <div className="faImages">
-              {/* ADD MOUSE OVER ANIMATION */}
-              <FaGithub
-                className="icon"
-                onClick={() =>
-                  this.handleLinkClick("https://github.com/squareben1")
-                }
-              />
-              <FaLinkedin
-                className="icon"
-                onClick={() =>
-                  this.handleLinkClick(
-                    "https://www.linkedin.com/in/bengittins/"
-                  )
-                }
-              />
+              <p>
+                Beatae minima, ipsam nisi rerum commodi. Culpa quod quibusdam,
+                odit ut! Reprehenderit officiis sint suscipit, neque, mollitia
+                minus? Ab earum cum nam at, quos id!
+              </p>
+              <div className="faImages">
+                {/* ADD MOUSE OVER ANIMATION */}
+                <FaGithub
+                  className="icon"
+                  onClick={() =>
+                    this.handleLinkClick("https://github.com/squareben1")
+                  }
+                />
+                <FaLinkedin
+                  className="icon"
+                  onClick={() =>
+                    this.handleLinkClick(
+                      "https://www.linkedin.com/in/bengittins/"
+                    )
+                  }
+                />
+              </div>
             </div>
           </div>
-          {/* ADD LANGUAGES/TECH */}
         </section>
       </>
     );
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,10 +1,37 @@
-import React from 'react'
-import { FaHtml5, FaCss3Alt, FaReact, FaPython, FaNode, FaSass, FaGitAlt, FaGithub, FaBootstrap } from 'react-icons/fa'
-import { DiDjango, DiMongodb, DiPostgresql, DiHeroku, DiVisualstudio, DiJsBadge } from "react-icons/di"
-import { GiCoffeeCup } from "react-icons/gi"
+import React from "react";
+import {
+  FaGem,
+  FaHtml5,
+  FaCss3Alt,
+  FaReact,
+  FaPython,
+  FaDatabase,
+  FaRecycle,
+  FaSass,
+  FaGitAlt,
+  FaGithub,
+  FaBootstrap,
+} from "react-icons/fa";
+import {
+  DiDjango,
+  DiMongodb,
+  DiPostgresql,
+  DiHeroku,
+  DiVisualstudio,
+  DiJsBadge,
+} from "react-icons/di";
+import { GiCoffeeCup } from "react-icons/gi";
 
 const Skills = () => (
   <div className="aboutSkill">
+    <div className="skillDiv">
+      <FaGem className="skillIcon" />
+      <p>Ruby & <br></br>Ruby on Rails</p>
+    </div>
+    <div className="skillDiv">
+      <FaPython className="skillIcon" />
+      <p>Python</p>
+    </div>
     <div className="skillDiv">
       <DiJsBadge className="skillIcon" />
       <p>JavaScript</p>
@@ -14,45 +41,31 @@ const Skills = () => (
       <p>React</p>
     </div>
     <div className="skillDiv">
-      <FaNode className="skillIcon" />
-      <p>Node.js</p>
-    </div>
-    <div className="skillDiv">
-      <FaPython className="skillIcon" />
-      <p>Python</p>
-    </div>
-    <div className="skillDiv">
-      <DiDjango className="skillIcon" />
-      <p>Django</p>
-    </div>
-    <div className="skillDiv">
-      < FaHtml5 className="skillIcon" />
-      <p>HTML5</p>
-    </div>
-    <div className="skillDiv">
-      < FaCss3Alt className="skillIcon" />
-      <p>CSS3</p>
-    </div>
-    <div className="skillDiv">
-      <FaSass className="skillIcon" />
-      <p>Sass</p>
-    </div>
-    <div className="skillDiv">
-      <FaBootstrap className="skillIcon" />
-      <p>Bootstrap</p>
-    </div>
-    <div className="skillDiv">
-      <GiCoffeeCup className="skillIcon" />
-      <p>Mocha</p>
-    </div>
-    <div className="skillDiv">
-      <DiMongodb className="skillIcon" />
-      <p>MongoDB</p>
+      <FaDatabase className="skillIcon" />
+      <p>SQL</p>
     </div>
     <div className="skillDiv">
       <DiPostgresql className="skillIcon" />
       <p>PostgreSQL</p>
     </div>
+    <div className="skillDiv">
+      <FaRecycle className="skillIcon" />
+      <p>
+        Test Driven<br></br>Development
+      </p>
+    </div>
+    <div className="skillDiv">
+      <FaHtml5 className="skillIcon" />
+      <p>HTML5</p>
+    </div>
+    <div className="skillDiv">
+      <FaCss3Alt className="skillIcon" />
+      <p>CSS3</p>
+    </div>
+    {/* <div className="skillDiv">
+      <FaSass className="skillIcon" />
+      <p>Sass</p>
+    </div> */}
     <div className="skillDiv">
       <DiHeroku className="skillIcon" />
       <p>Heroku</p>
@@ -65,11 +78,11 @@ const Skills = () => (
       <FaGithub className="skillIcon" />
       <p>GitHub</p>
     </div>
-    <div className="skillDiv">
+    {/* <div className="skillDiv">
       <DiVisualstudio className="skillIcon" />
       <p>VS Code</p>
-    </div>
+    </div> */}
   </div>
-)
+);
 
-export default Skills
+export default Skills;
