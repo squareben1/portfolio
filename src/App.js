abfbadc1e12c6d5f5c9976ac1c1f8ab505c774ca
Squareben1 <bengittins@live.com>
2020-07-02 21:38:35 +0100

    Deploy prep

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from "react";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import NavBar from "./components/NavBar";
 import Contact from "./components/Contact";
 import Projects from "./components/Projects";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 import "./styles/main.scss";
 
 function App() {
   return (
     <div className="App">
+      {/* <HashRouter> */}
       <header className="App-header">
+        {/* <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/projects" component={Projects} />
+            <Route exact path="/contact" component={Contact} />
+
+          </Switch> */}
         <NavBar />
         <Home />
         <About />
@@ -18,6 +27,7 @@ function App() {
         <Contact />
         <Footer />
       </header>
+      {/* </HashRouter> */}
     </div>
   );
 }
