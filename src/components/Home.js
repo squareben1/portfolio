48c37cf1f21c28e7f89bb1af6867e9ce050ec24d
Ben_Gittins_personal <ben_gittins_personal@new-host-5.home>
2021-11-20 17:41:05 -0800

    Change word. test pipeline.

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,7 +13,7 @@ class Home extends React.Component {
           <div className="homeTextContainer">
             Hi, I'm Ben.
             <br></br>
-            I’m a Site Reliability Engineer.
+            I’m a Site Reliability Engineer & Developer.
           </div>
           <div className="profilePic">
             <img src={profilePic} alt="profilePic" />
